11b57aaf0c1c0c3723ce2a8f6372735328574cf8
nurhossainfarid <faahsan1516@gmail.com>
2023-07-12 17:11:49 +0600

    update admin attendance model

--- models/AdminAttendanceModel.js
+++ models/AdminAttendanceModel.js
@@ -3,14 +3,20 @@ const { Schema, model } = require('mongoose');
 const adminAttendanceSchema = new Schema({
     timeLimit: {
         type: Number,
+        required: [true, "Time limit is required"],
+        min: 5,
+        max: 30,
+        default: 5,
     },
     status: {
         type: String,
-    },
-    createdAt: {
-        type: Date,
+        required: [true, "Status is required"],
+        enum: ["RUNNING", "COMPLETED"],
+        default: "RUNNING",
     }
-});
+},
+    { timestamps: true }
+);
 
 const AdminAttendance = model('AdminAttendance', adminAttendanceSchema);
 module.exports = AdminAttendance;
